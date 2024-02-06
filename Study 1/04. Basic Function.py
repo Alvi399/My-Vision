076e1961e2587d1f9105dc83c9a6b78956a56eb3
Alvi399 <muhammadalvi.23339@mhs.unesa.ac.id>
2024-02-06 08:54:45 +0700

    up

--- Study 1/04. Basic Function.py
+++ Study 1/04. Basic Function.py
@@ -1,8 +1,9 @@
 import cv2 
 import numpy as np
+import os
 
-
-img = cv2.imread('src/koin.jpg')
+print("Current Working Directory:", os.getcwd())
+img = cv2.imread("Study 1/src//koin.jpg",flags=1)
 
 if img is None:
     print('Failed to load image')
